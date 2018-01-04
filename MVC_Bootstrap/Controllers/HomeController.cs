891478c0ff5dc49b63817a923740bd83d4bc78da
Bishode123 <34882617+Bishode123@users.noreply.github.com>
2018-01-04 12:52:30 +0530

    Add project files.

---
+++ MVC_Bootstrap/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_Bootstrap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Bootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home
+        [HttpPost]
+        public ActionResult Index(Employee e1)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ThankYou");
+            }
+            return View(e1);
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVC_Bootstrap/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Bootstrap.Models
+{
+    public class Employee
+    {
+        [DisplayName("Enter Your First Name")]
+        [Required(ErrorMessage = "Please Enter First Name!!!!!!!!!!")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Enter Your Last Name")]
+        [Required(ErrorMessage = "Please Enter Last Name!!!!!!!!!!")]
+        public string LastName { get; set; }
+
+        [DisplayName("Enter Your Age")]
+        [Required(ErrorMessage = "Please Enter Age!!!!!!!!!!")]
+        public int Age { get; set; }
+    }
+}
